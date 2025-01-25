629dda895e823bf8306a6417c8d4109f65e11f9c
Egor Kuznetsov <egorkuz1408@gmail.com>
2025-01-25 14:21:27 +0400

    Added dataBinding to project Implemented bindingAdapters

--- app/src/main/java/com/kzerk/compositionapp/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/kzerk/compositionapp/presentation/GameFinishedFragment.kt
@@ -1,19 +1,13 @@
 package com.kzerk.compositionapp.presentation
 
-import android.annotation.SuppressLint
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.kzerk.compositionapp.R
 import com.kzerk.compositionapp.databinding.FragmentGameFinishedBinding
-import com.kzerk.compositionapp.domain.entity.GameResult
 
 
 class GameFinishedFragment : Fragment() {
@@ -34,49 +28,12 @@ class GameFinishedFragment : Fragment() {
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 		setupOnClickListeners()
-		bindViews()
+		binding.gameResult = args.gameResult
 	}
 
 	private fun setupOnClickListeners() {
 		binding.buttonRetry.setOnClickListener {
 			retryGame()
-		}
-	}
-
-	@SuppressLint("StringFormatMatches")
-	private fun bindViews() = with(binding) {
-		emojiResult.setImageResource(getSmileResId())
-		tvRequiredAnswers.text = String.format(
-			getString(R.string.required_score),
-			args.gameResult.gameSettings.minCountOfRightAnswers
-		)
-		tvScoreAnswers.text = String.format(
-			getString(R.string.score_answers),
-			args.gameResult.countOfRightAnswers
-		)
-		tvRequiredPercentage.text = String.format(
-			getString(R.string.required_percentage),
-			args.gameResult.gameSettings.minPercentOfRightAnswers
-		)
-		tvScorePercentage.text = String.format(
-			getString(R.string.score_percentage),
-			getPercentOfRightAnswers()
-		)
-	}
-
-	private fun getSmileResId(): Int {
-		return if (args.gameResult.winner) {
-			R.drawable.ic_smile
-		} else {
-			R.drawable.ic_sad
-		}
-	}
-
-	private fun getPercentOfRightAnswers() =  with(args.gameResult) {
-		if (countOfQuestions == 0) {
-			0
-		} else {
-			((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
 		}
 	}
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
 	alias(libs.plugins.kotlin.parcelize) apply false
 	alias(libs.plugins.com.google.devtools.ksp) apply false
 	alias(libs.plugins.navigation.safeargs.kotlin) apply false
+	alias(libs.plugins.kotlin.kapt) apply false
 }
